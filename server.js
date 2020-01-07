086258946b263a8f438ca1ed595439c9686d3aca
Maciej Kryczka <maciejkryczka@hotmail.com>
2020-01-07 16:37:57 +0100

    create one server for both app and api

--- server.js
+++ server.js
@@ -14,14 +14,21 @@ app.use(cors({
   "origin": "http://localhost:3000",
 }));
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.use('/api', testimonialsRoutes);
 app.use('/api', seatsRoutes);
 app.use('/api', concertsRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 app.use((req, res) => {
   res.status(404).send({message: '404 not found...'});
 })
 
-app.listen(8000, () => {
-  console.log('server is running on port: 8000');
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
 });
