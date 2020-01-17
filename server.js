592a34381a3145f33f04c6dd03a08a75d7c8463a
Maciej Kryczka <maciejkryczka@hotmail.com>
2020-01-17 13:41:15 +0100

    add mongoose database and models

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const socket = require('socket.io');
 const path = require('path');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const seatsRoutes = require('./routes/seats.routes');
@@ -34,6 +35,14 @@ app.get('*', (req, res) => {
 app.use((req, res) => {
   res.status(404).send({message: '404 not found...'});
 })
+
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', err => console.log('Error ' + err));
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000 ');
